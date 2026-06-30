661caef55b8db977232dd099991f8570f15d0061
matricarin <matricarin@gmail.com>
2026-06-30 21:08:03 +0500

    добавлен параметр для координат

---
+++ src/ImageTransformer/Parameters/Coordinates.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageTransformer.Parameters;
+
+public readonly record struct Coordinates(int X, int Y, int Width, int Height)
+{
+    public static bool TryParse(string value, out Coordinates result)
+    {
+        throw new NotImplementedException();
+    }
+}
