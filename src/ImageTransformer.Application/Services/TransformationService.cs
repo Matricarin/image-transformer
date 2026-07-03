b854ae9d48657c1b0c7d43050fd1ed3f7c994975
matricarin <matricarin@gmail.com>
2026-07-03 15:21:21 +0500

    transforamtion mmethods were added

--- src/ImageTransformer.Application/Services/TransformationService.cs
+++ src/ImageTransformer.Application/Services/TransformationService.cs
@@ -1,12 +1,48 @@
 ﻿using ImageTransformer.Application.Interfaces;
 using ImageTransformer.Application.Models;
+using SkiaSharp;
 
 namespace ImageTransformer.Application.Services;
 
 public sealed class TransformationService : ITransformationService
 {
+    private const int NinetyDegrees = 90;
     public void Transform(TransformationType transformation)
     {
-        throw new NotImplementedException();
+        //var bitmap = SKBitmap.Decode(bytes); нужно передать span bytes
+
+        //var bitmap = new SKBitmap();
+
+        //var info = bitmap.Info;
+        //using (var surface = SKSurface.Create(info))
+        //{
+        //    var canvas = surface.Canvas;
+        //}
+    }
+
+    private SKCanvas RotateClockwise(SKCanvas canvas)
+    {
+        canvas.RotateDegrees(NinetyDegrees);
+        return canvas;
+    }
+
+    private SKCanvas RotateCounterClockwise(SKCanvas canvas)
+    {
+        canvas.RotateDegrees(-NinetyDegrees);
+        return canvas;
+    }
+
+    private SKCanvas FlipHorizontally(SKCanvas canvas, int width)
+    {
+        canvas.Scale(-1, 1);
+        canvas.Translate(-width, 0);
+        return canvas;
+    }
+
+    private SKCanvas FlipVertically(SKCanvas canvas, int height)
+    {
+        canvas.Scale(1, -1);
+        canvas.Translate(0, -height);
+        return canvas;
     }
 }
