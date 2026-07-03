6bef910f24b2d18b8f4b7ab8de86cdce3937b113
matricarin <matricarin@gmail.com>
2026-07-03 17:51:22 +0500

    updated

--- src/ImageTransformer/Apis/ImageTransformerApi.cs
+++ src/ImageTransformer/Apis/ImageTransformerApi.cs
@@ -1,10 +1,14 @@
 ﻿using ImageTransformer.Apis.Parameters;
 using ImageTransformer.Models;
+using SkiaSharp;
 
 namespace ImageTransformer.Apis;
 
 public static class ImageTransformerApi
 {
+    private const long MaxFileSizeBytes = 100 * 1024; 
+    private const int MaxDimension = 1000;
+
     public static RouteHandlerBuilder MapImageTransformApi(this IEndpointRouteBuilder app)
     {
         var api = app.MapPost("/process/{transform}/{coords}", ProcessImage);
@@ -29,9 +33,49 @@ public static class ImageTransformerApi
             return Results.BadRequest();
         }
 
+        if (context.Request.ContentLength > MaxFileSizeBytes)
+        {
+            return Results.BadRequest();
+        }
+
         using (var memory = new MemoryStream())
         {
             await context.Request.Body.CopyToAsync(memory);
+
+            if (memory.Length > MaxFileSizeBytes)
+            {
+                return Results.BadRequest();
+            }
+
+            memory.Position = 0;
+
+            using (var skStream = new SKManagedStream(memory))
+            {
+                using (var codec = SKCodec.Create(skStream))
+                {
+                    if (codec is null)
+                    {
+                        return Results.BadRequest();
+                    }
+
+                    var info = codec.Info;
+
+                    if (info.Width > MaxDimension || info.Height > MaxDimension)
+                    {
+                        return Results.BadRequest();
+                    }
+
+                    if (info.ColorType != SKColorType.Bgra8888)
+                    {
+                        return Results.BadRequest();
+                    }
+                }
+            }
+            memory.Position = 0;
+
+            var bytes = new ReadOnlySpan<byte>(memory.ToArray());
+
+            throw new NotImplementedException();
         }
     }
 }
