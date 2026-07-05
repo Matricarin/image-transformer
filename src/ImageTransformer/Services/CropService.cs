25e893cc8618c4662733e09b46df42573b0d6da4
matricarin <matricarin@gmail.com>
2026-07-05 10:18:19 +0500

    memory leaks was removed

--- src/ImageTransformer/Services/CropService.cs
+++ src/ImageTransformer/Services/CropService.cs
@@ -8,11 +8,11 @@ public sealed class CropService : ICropService
 {
     public ReadOnlySpan<byte> Crop(Coordinates coords, ReadOnlySpan<byte> bitmapBytes)
     {
-        var sourceBitmap = SKBitmap.Decode(bitmapBytes);
+        using var sourceBitmap = SKBitmap.Decode(bitmapBytes);
 
         var destRect = new SKRect(0, 0, coords.Width, coords.Height);
 
-        var destBitmap = new SKBitmap(coords.Width, coords.Height);
+        using var destBitmap = new SKBitmap(coords.Width, coords.Height);
 
         var sourceRect = new SKRect
         (
--- src/ImageTransformer/Services/TransformationService.cs
+++ src/ImageTransformer/Services/TransformationService.cs
@@ -10,42 +10,44 @@ public sealed class TransformationService : ITransformationService
 
     public ReadOnlySpan<byte> Transform(TransformationType transformation, ReadOnlySpan<byte> bitmapBytes)
     {
-        var bitmap = SKBitmap.Decode(bitmapBytes);
+        using var bitmap = SKBitmap.Decode(bitmapBytes);
 
         var info = bitmap.Info;
 
-        using (var surface = SKSurface.Create(info))
+        using var surface = SKSurface.Create(info);
+
+        var canvas = surface.Canvas;
+
+        canvas.Save();
+        
+        switch (transformation)
         {
-            var canvas = surface.Canvas;
-            canvas.Save();
-            switch (transformation)
-            {
-                case TransformationType.RotateCounterClockwise:
+            case TransformationType.RotateCounterClockwise:
                 {
                     RotateClockwise(canvas);
                     break;
                 }
-                case TransformationType.RotateClockwise:
+            case TransformationType.RotateClockwise:
                 {
                     RotateCounterClockwise(canvas);
                     break;
                 }
-                case TransformationType.FlipVertically:
+            case TransformationType.FlipVertically:
                 {
                     FlipVertically(canvas, bitmap.Height);
                     break;
                 }
-                case TransformationType.FlipHorizontally:
+            case TransformationType.FlipHorizontally:
                 {
                     FlipHorizontally(canvas, bitmap.Width);
                     break;
                 }
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(transformation), transformation, null);
-            }
-
-            canvas.Restore();
+            default:
+                throw new ArgumentOutOfRangeException(nameof(transformation), transformation, null);
         }
+
+        canvas.Restore();
+
 
         return bitmapBytes;
     }
