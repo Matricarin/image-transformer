80496d9a6e5e1625f2be290f5f8c0ba914be5a10
matricarin <matricarin@gmail.com>
2026-07-05 20:05:09 +0500

    test data generation

---
+++ test/ImageTransformer.Tests/TestData/TestBitmapFactory.cs
@@ -0,0 +1,25 @@
+﻿using SkiaSharp;
+
+namespace ImageTransformer.Tests.TestData;
+
+public static class TestBitmapFactory
+{
+
+    public static SKBitmap CreateRedBlueSquare(TestTwoColorsSquare squareData)
+    {
+        var source = new SKBitmap(squareData.Width, squareData.Height, SKColorType.Bgra8888, SKAlphaType.Opaque);
+
+        var canvas = new SKCanvas(source);
+
+        canvas.Save();
+
+        canvas.DrawPoint(0, 0, SKColor.Parse(squareData.FirstColorHexString));
+        canvas.DrawPoint(0, 1, SKColor.Parse(squareData.FirstColorHexString));
+        canvas.DrawPoint(1, 0, SKColor.Parse(squareData.SecondColorHexString));
+        canvas.DrawPoint(1, 1, SKColor.Parse(squareData.SecondColorHexString));
+
+        canvas.Restore();
+
+        return source;
+    }
+}
---
+++ test/ImageTransformer.Tests/TestData/TestTwoColorsSquare.cs
@@ -0,0 +1,27 @@
+﻿using SkiaSharp;
+
+namespace ImageTransformer.Tests.TestData;
+
+public sealed class TestTwoColorsSquare
+{
+    public string FirstColorHexString { get; }
+    public string SecondColorHexString { get; }
+    public int Width { get; }
+    public int Height { get; }
+    public SKColor FirstColor => SKColor.Parse(FirstColorHexString);
+    public SKColor SecondColor => SKColor.Parse(SecondColorHexString);
+
+    public TestTwoColorsSquare
+    (
+        string firstColorHexString,
+        string secondColorHexString,
+        int width,
+        int height
+    )
+    {
+        FirstColorHexString = firstColorHexString;
+        SecondColorHexString = secondColorHexString;
+        Width = width;
+        Height = height;
+    }
+}
