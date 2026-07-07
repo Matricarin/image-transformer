33fb0f31e6411d6b88f7972e1e07834d2ae1bdbf
matricarin <matricarin@gmail.com>
2026-07-07 16:54:07 +0500

    parsing was been implemented

--- src/ImageTransformer/Models/Coordinates.cs
+++ src/ImageTransformer/Models/Coordinates.cs
@@ -4,6 +4,36 @@ public readonly record struct Coordinates(int X, int Y, int Width, int Height)
 {
     public static bool TryParse(string value, out Coordinates result)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(value))
+        {
+            result = new Coordinates();
+            return false;
+        }
+
+        var strings = value.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+        if (strings.Length != 4)
+        {
+            result = new Coordinates();
+            return false;
+        }
+
+        var numbers = new int[4];
+
+        for (var i = 0; i < 4; i++)
+        {
+            if (int.TryParse(strings[i], out var num))
+            {
+                numbers[i] = num;
+            }
+            else
+            {
+                result = new Coordinates();
+                return false;
+            }
+        }
+
+        result = new Coordinates(numbers[0], numbers[1], numbers[2], numbers[3]);
+        return true;
     }
 }
