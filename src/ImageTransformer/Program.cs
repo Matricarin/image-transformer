30f766eaae26a90e2bf78e13e0453e2a2fb61853
matricarin <matricarin@gmail.com>
2026-08-07 17:46:03 +0500

    opentelemetry was added

--- src/ImageTransformer/Program.cs
+++ src/ImageTransformer/Program.cs
@@ -1,11 +1,23 @@
 using ImageTransformer.Apis;
 using ImageTransformer.Services;
+using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddImageTransformServices();
 
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(metrics =>
+    {
+        metrics.AddAspNetCoreInstrumentation()
+            .AddRuntimeInstrumentation()
+            .AddMeter(ApplicationDiagnostics.ServiceName)
+            .AddPrometheusExporter();
+    });
+
 var app = builder.Build();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 app.UseHttpsRedirection();
 
